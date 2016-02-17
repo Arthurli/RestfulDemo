78d9bc4aaf9e38c0f6dada57597d86ddbe395dd9
lichen <arthurli1991@gmail.com>
2016-02-17 16:28:51 +0800

    Add unit test

---
+++ Database/user_test.go
@@ -0,0 +1,109 @@
+package Database
+
+import (
+	"testing"
+
+	"RestfulDemo/Model"
+	pg "gopkg.in/pg.v3"
+)
+
+var testUserDB *UserData
+var user1, user2 *Model.User
+var err error
+
+func Test_Init(t *testing.T) {
+	db := pg.Connect(&pg.Options{
+		User: "test",
+	})
+	user := new(UserData)
+	user.db = db
+	err = user.Init()
+	if err != nil {
+		t.Fatal(err.Error())
+		return
+	}
+	testUserDB = user
+}
+
+func Test_CreateUser(t *testing.T) {
+	if testUserDB == nil {
+		return
+	}
+
+	user1, err = testUserDB.InsertUser("user1")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	user2, err = testUserDB.InsertUser("user2")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+}
+
+func Test_GetUsers(t *testing.T) {
+	if testUserDB == nil {
+		return
+	}
+
+	_, err = testUserDB.GetUsers()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+}
+func Test_InsertRelation(t *testing.T) {
+	if testUserDB == nil ||
+		user1 == nil ||
+		user2 == nil {
+		return
+	}
+
+	r, err := testUserDB.InsertRelation(user1.Id, user2.Id, 1)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if r.Relation != 1 {
+		t.Fatal("Insert error")
+	}
+}
+func Test_UpdateRelation(t *testing.T) {
+	if testUserDB == nil ||
+		user1 == nil ||
+		user2 == nil {
+		return
+	}
+
+	r, err := testUserDB.UpdateRelation(user1.Id, user2.Id, -1)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if r.Relation != -1 {
+		t.Fatal("Update error")
+	}
+}
+func Test_GetRelation(t *testing.T) {
+	if testUserDB == nil ||
+		user1 == nil ||
+		user2 == nil {
+		return
+	}
+
+	_, err = testUserDB.GetRelation(user1.Id, user2.Id)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+}
+func Test_GetRelations(t *testing.T) {
+	if testUserDB == nil ||
+		user1 == nil ||
+		user2 == nil {
+		return
+	}
+
+	_, err = testUserDB.GetRelations(user1.Id)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+}
---
+++ Service/user_test.go
@@ -0,0 +1,152 @@
+package Service
+
+import (
+	"testing"
+
+	"RestfulDemo/Model"
+)
+
+var userService *UserService
+
+func Test_Init(t *testing.T) {
+	userService = new(UserService)
+	userService.userDatabase = new(TestUserDB)
+}
+
+func Test_GetUsers(t *testing.T) {
+	users, err := userService.GetUsers()
+
+	if err != nil {
+		t.Fatal(err.Error())
+		return
+	}
+
+	if len(users) != 1 {
+		t.Fatal("Fetch users length error")
+	}
+}
+
+func Test_CreateUser(t *testing.T) {
+	user, err := userService.userDatabase.InsertUser("test")
+
+	if err != nil {
+		t.Fatal(err.Error())
+		return
+	}
+
+	if user == nil {
+		t.Fatal("Create user fatal")
+		return
+	}
+
+	if user.Name != "test" {
+		t.Fatal("New user's name is wrong")
+	}
+}
+
+func Test_GetUserRelations(t *testing.T) {
+	rs, err := userService.GetUserRelations(1)
+
+	if err != nil {
+		t.Fatal(err.Error())
+		return
+	}
+
+	if len(rs) != 1 {
+		t.Fatal("Fetch relations length error")
+	}
+}
+
+func Test_ChangeUserRelation(t *testing.T) {
+	r, err := userService.ChangeUserRelation(1, 2, 1)
+	if err != nil {
+		t.Fatal(err.Error())
+		return
+	}
+
+	if r.Relation != 2 {
+		t.Fatal("Insert user relation wrong")
+		return
+	}
+
+	r, err = userService.ChangeUserRelation(1, 2, -1)
+	if err != nil {
+		t.Fatal(err.Error())
+		return
+	}
+
+	if r.Relation != -1 {
+		t.Fatal("Update user relation wrong")
+	}
+}
+
+type TestUserDB struct {
+}
+
+func (t *TestUserDB) GetUsers() ([]*Model.User, error) {
+	var users = make([]*Model.User, 0, 0)
+	users = append(users, &Model.User{
+		Id:   1,
+		Name: "test",
+		Type: "user",
+	})
+	return users, nil
+}
+func (t *TestUserDB) InsertUser(name string) (*Model.User, error) {
+	return &Model.User{
+		Id:   1,
+		Name: name,
+		Type: "user",
+	}, nil
+}
+func (t *TestUserDB) GetRelation(userid int64, targetid int64) (*Model.Relation, error) {
+
+	var r *Model.Relation
+
+	if userid == 1 && targetid == 2 {
+		r = &Model.Relation{
+			Id:       1,
+			UserId:   1,
+			TargetId: 2,
+			Relation: 1,
+		}
+	} else if userid == 2 && targetid == 1 {
+		r = &Model.Relation{
+			Id:       2,
+			UserId:   2,
+			TargetId: 1,
+			Relation: 1,
+		}
+	}
+
+	return r, nil
+}
+func (t *TestUserDB) GetRelations(userid int64) ([]*Model.Relation, error) {
+	var rs = make([]*Model.Relation, 0, 0)
+	if userid == 1 {
+		rs = append(rs, &Model.Relation{
+			Id:       1,
+			UserId:   1,
+			TargetId: 2,
+			Relation: 1,
+		})
+	}
+
+	return rs, nil
+}
+func (t *TestUserDB) InsertRelation(userid int64, targetid int64, relation int64) (*Model.Relation, error) {
+	return &Model.Relation{
+		Id:       1,
+		UserId:   userid,
+		TargetId: targetid,
+		Relation: relation,
+	}, nil
+}
+func (t *TestUserDB) UpdateRelation(userid int64, targetid int64, relation int64) (*Model.Relation, error) {
+	return &Model.Relation{
+		Id:       1,
+		UserId:   userid,
+		TargetId: targetid,
+		Relation: relation,
+	}, nil
+}
